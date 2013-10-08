799f3cd61d97ca6b8d71695ea16d36023250d4d2
QingYun <qingyun95@gmail.com>
2013-10-08 16:26:04 +0800

    fix CallCountRecorder

--- tracer/call_count_recorder.hpp
+++ tracer/call_count_recorder.hpp
@@ -5,18 +5,18 @@ namespace tracer {
 //! 记录函数调用次数的记录器
 template<typename T>
 class CallCountRecorder {
-	T &tracer_;
 	Connection conn_;
 	std::size_t count_;
 
 public:
 	CallCountRecorder(T &tracer) :
-		tracer_(tracer),
 		count_(0) {
-		conn_ = tracer_.Before().connect_without_params([this] () mutable {
+		conn_ = tracer.Before().connect_without_params([this] () mutable {
 			count_++;
 		});
 	}
+	~CallCountRecorder() { conn_.disconnect(); }
+
 	bool HasBeenCalled() const { return !!count_; }
 	std::size_t CallCount() const { return count_; }
 };
